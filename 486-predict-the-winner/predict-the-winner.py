cf549d522aecd291b90cb68ac73ae82ab3cd53be
Nikhil George <156281709+NikhilGeorge01@users.noreply.github.com>
2026-08-01 07:23:24 +0530

    Time: 3 ms (55.77%) | Memory: 20 MB (11.54%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,10 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        @cache
+        def dfs(i,j):
+            if i > j:
+                return 0
+            if i == j:
+                return nums[i]
+            return max(nums[i] - dfs(i+1,j) , nums[j] - dfs(i,j-1))
+        return dfs(0,len(nums) - 1) >= 0
