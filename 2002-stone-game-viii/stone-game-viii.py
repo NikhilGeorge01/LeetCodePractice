d4d123e5d9d2a50c1b0ede9b77be84aa87ef956c
Nikhil George <156281709+NikhilGeorge01@users.noreply.github.com>
2026-08-24 08:28:11 +0530

    Time: 685 ms (52.66%) | Memory: 33.3 MB (39.64%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.py
+++ 2002-stone-game-viii/stone-game-viii.py
@@ -3,9 +3,9 @@ class Solution:
         cumsum = [0]
         for x in stones:
             cumsum.append(cumsum[-1] + x)
-        @cache
-        def dfs(i):
-            if i == len(stones):
-                return cumsum[-1]
-            return max(cumsum[i] - dfs(i + 1), dfs(i + 1))
-        return dfs(2)
+        n = len(stones)
+        dp = [0] * (n + 1)
+        dp[n] = cumsum[n]
+        for i in range(n - 1, 1, -1):
+            dp[i] = max(cumsum[i] - dp[i + 1], dp[i + 1])
+        return dp[2]
