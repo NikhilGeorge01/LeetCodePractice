650c902cbf54868e18abd73a576bde437017d2ee
Nikhil George <156281709+NikhilGeorge01@users.noreply.github.com>
2026-09-03 19:32:03 +0530

    Time: 12 ms (87.80%) | Memory: 35.2 MB (73.17%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        #if smallest even then all even 
-        #if smallest odd return True
         a = min(nums1)
         if a % 2 == 1:
             return True
