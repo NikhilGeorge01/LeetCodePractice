7d6471bfe5e5aa202d55c24dd4be7b6972ed5e75
Nikhil George <156281709+NikhilGeorge01@users.noreply.github.com>
2026-09-10 07:50:22 +0530

    Time: 63 ms (10.24%) | Memory: 22.4 MB (10.24%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
@@ -6,16 +6,19 @@
 #         self.right = right
 class Solution:
     def averageOfSubtree(self, root: TreeNode) -> int:
+        @cache
         def ct(node):
             if not node:
                 return 0
             else:
                 return 1 + ct(node.left) + ct(node.right)
+        @cache
         def sm(node):
             if not node:
                 return 0
             else:
                 return node.val + sm(node.left) + sm(node.right)
+        @cache
         def av(node):
             if not node:
                 return 0
