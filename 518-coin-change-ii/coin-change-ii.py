d26aceb7cf998fe6d82a35f6c6e49fbc5791b5ca
Nikhil George <156281709+NikhilGeorge01@users.noreply.github.com>
2026-09-15 09:21:55 +0530

    Time: 464 ms (44.38%) | Memory: 19.4 MB (83.71%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -3,12 +3,11 @@ class Solution:
         dp = [0 for i in range(amount+1)]
         dp[-1] = 1
         for i in range(len(coins)):
-            temp = dp.copy()
             for j in range(len(dp) - 1, -1, -1):
                 a = j + coins[i]
                 if a >= len(dp):
                     a = 0
                 else:
                     a = dp[j + coins[i]]
-                dp[j] = a + temp[j]
+                dp[j] = a + dp[j]
         return dp[0]
